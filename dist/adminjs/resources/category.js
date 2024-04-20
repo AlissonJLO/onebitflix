cd272a9cb42d39ff5a0d93b344648f769d218e59
Alisson Joaquim lara oliveira <alissonloaquim@hotmail.com>
2024-04-20 11:34:34 -0400

    delete:dist

--- dist/adminjs/resources/category.js
+++
@@ -1,7 +0,0 @@
-export const categoryResourceOptions = {
-    navigation: "Catálogo",
-    editProperties: ["name", "position"],
-    filterProperties: ["name", "position", "createdAt", "updatedAt"],
-    listProperties: ["id", "name", "position"],
-    showProperties: ["id", "name", "position", "createdAt", "updatedAt"],
-};
