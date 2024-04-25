f7d8ab9f9fafbf4ee67bd0a17619ec4694c01238
Alisson Joaquim lara oliveira <alissonloaquim@hotmail.com>
2024-04-24 21:33:49 -0400

    add:rota de categorias

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+// src/routes.ts
+
+import express from "express";
+import { categoriesController } from "./controllers/categoriesController.js";
+
+const router = express.Router();
+
+router.get("/categories", categoriesController.index);
+
+export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import database from "./database/index.js";
 import { adminJs, adminJsRouter } from "./adminjs/index.js";
+import { router } from "./routes.js"; 
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(router);
 
 app.use(adminJs.options.rootPath, adminJsRouter);
 
