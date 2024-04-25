eb3be92b9fc58c36633060926724a29ee01f6b72
Alisson Joaquim lara oliveira <alissonloaquim@hotmail.com>
2024-04-24 23:29:02 -0400

    add:categoria por id com cursos associados

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,9 +3,9 @@
 import { Category } from "./Category.js";
 import { Course } from "./Course.js";
 import { Episode } from "./Episode.js";
-import { User } from './User.js';
+import { User } from "./User.js";
 
-Category.hasMany(Course);
+Category.hasMany(Course, { as: "courses" });
 
 Course.belongsTo(Category);
 Course.hasMany(Episode);
